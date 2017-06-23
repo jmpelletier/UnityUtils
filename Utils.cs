d57f9f66337abb054f8426f998969a226514ac0c
Jean-Marc Pelletier <jmp@jmpelletier.com>
2017-06-23 09:46:32 +0900

    Initial commit

---
+++ Utils.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Utils  {
+
+	public static T LazyLoad<T>(ref T v) where T:MonoBehaviour {
+		if (v == null) {
+			GameObject o = new GameObject(typeof(T).Name);
+			v = o.AddComponent<T>();
+		}
+		return v;
+	}
+	
+}
---
+++ Watch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Watch : MonoBehaviour {
+
+	private static Watch _instance;
+	private static Watch instance {
+		get {
+			return Utils.LazyLoad(ref _instance);
+		}
+	}
+}
